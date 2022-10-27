eed45847e5ba7ef03232fff373af633027dcebfc
KayhanOgan <kayhan.ogan@hbz-nrw.de>
2022-10-27 14:17:25 +0200

    typo

--- app/helper/LRMIMapper.java
+++ app/helper/LRMIMapper.java
@@ -452,7 +452,7 @@ public class LRMIMapper {
 						// Affiliation element should be omitted, if no specification is
 						// made.
 						String affilId = affiliation.get(i);
-						if (!affilName.equals("http://hbz-nrw.de/regal#affiliation/unknown")) {
+						if (!affilId.equals("http://hbz-nrw.de/regal#affiliation/unknown")) {
 							obj.put("affiliation", affObj);
 						}
 					} else {
