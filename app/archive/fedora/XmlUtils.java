cac0c78482e42bee576a1cddb75f6725393cf961
pellerito <alessio.pellerito@hbz-nrw.de>
2022-12-01 11:17:02 +0100

    added transabstract tag

--- app/archive/fedora/XmlUtils.java
+++ app/archive/fedora/XmlUtils.java
@@ -761,26 +761,42 @@ public class XmlUtils {
 			rdf.put("license", licenses);
 
 			/* Abstract und Trans-abstract */
+			List<String> abstracts = new ArrayList<>();
+
 			nodeList = content.getElementsByTagName("abstract");
 			if (nodeList.getLength() > 0) {
 				Node paragraphNode = getFirstElementNode(nodeList.item(0));
 				Node boldNode = getFirstElementNode(paragraphNode);
-				if (boldNode != null) {
-					if (boldNode.getNodeName().equalsIgnoreCase("bold"))
-						paragraphNode.removeChild(boldNode);
+				if (boldNode != null
+						&& boldNode.getNodeName().equalsIgnoreCase("bold")) {
+					paragraphNode.removeChild(boldNode);
 				}
-				rdf.put("abstractText",
-						Arrays.asList(paragraphNode.getTextContent().trim()
-								.replaceAll("[\\r\\n\\t\\u00a0]+", " ")
-								.replaceAll("\\s+", " ")));
+				abstracts.add(paragraphNode.getTextContent().trim()
+						.replaceAll("[\\r\\n\\t\\u00a0]+", " ").replaceAll("\\s+", " "));
 			}
-			/*
-			 * nodeList = content.getElementsByTagName("trans-abstract"); if
-			 * (nodeList.getLength() > 0) { Node titleNode =
-			 * getFirstElementNode(nodeList.item(0)); if(titleNode != null) { if
-			 * (titleNode.getNodeName().equalsIgnoreCase("title"))
-			 * nodeList.item(0).removeChild(titleNode); } }
-			 */
+
+			nodeList = content.getElementsByTagName("trans-abstract");
+			if (nodeList.getLength() > 0) {
+				childNodes = nodeList.item(0).getChildNodes();
+				for (int i = 0; i < childNodes.getLength(); i++) {
+					Node childNode = childNodes.item(i);
+					if (childNode.getNodeName().equalsIgnoreCase("title")) {
+						continue;
+					}
+					if (childNode.getNodeName().equalsIgnoreCase("p")) {
+						Node boldNode = getFirstElementNode(childNode);
+						if (boldNode != null
+								&& boldNode.getNodeName().equalsIgnoreCase("bold")) {
+							childNode.removeChild(boldNode);
+						}
+						abstracts.add(childNode.getTextContent().trim()
+								.replaceAll("[\\r\\n\\t\\u00a0]+", " ")
+								.replaceAll("\\s+", " "));
+					}
+				}
+			}
+
+			rdf.put("abstractText", abstracts);
 
 			/* Schlagwörter */
 			nodeList = content.getElementsByTagName("kwd");
