7181c0143f67f31a66bc9d1328b28e65e0f57425
hadoud <112628792+hadoud@users.noreply.github.com>
2023-01-27 15:54:34 +0100

    redefine method Metadata2Helper.getMetadata2ByToScienceJson() for the case mapping of metadata2

--- app/helper/Metadata2Helper.java
+++ app/helper/Metadata2Helper.java
@@ -31,6 +31,11 @@ public class Metadata2Helper {
 			String toscienceJsonContent) {
 		try {
 
+			if (toscienceJsonContent == null) {
+				play.Logger.debug("toscienceJsonContent is empty");
+				return null;
+			}
+
 			JsonMapper jsmapper = new JsonMapper();
 			jsmapper.node = n;
 
@@ -38,69 +43,51 @@ public class Metadata2Helper {
 
 			JSONArray jArr = null;
 			JSONObject jObj = null;
-			Object myObj = null;
 			Map<String, Object> rdf = n.getLd2();
 
+			play.Logger.debug("rdf = " + rdf.toString());
+
 			JSONObject toscienceJsonObject = new JSONObject(toscienceJsonContent);
-			LinkedHashMap<String, Object> metadata2Map = new LinkedHashMap();
+
+			LinkedHashMap<String, Object> metadata2Map = new LinkedHashMap<>();
+
+			play.Logger.debug(
+					"getMetadata2ByToScienceJson() LinkedHashMap has been created");
 
 			if (toscienceJsonObject.has("@context")) {
-				jArr = toscienceJsonObject.getJSONArray("@context");
-				play.Logger.debug("Found context: " + jArr.getString(0));
-				rdf.put("@context", jArr.getString(0));
-				jObj = jArr.getJSONObject(1);
-				String language = jObj.getString("@language");
-				play.Logger.debug("Found language: " + language);
+				jObj = toscienceJsonObject.getJSONObject("@context");
+				rdf.put("@context", jObj.toString());
+			}
 
-				// Mapping of Language with Locale.class as helper
-				Map<String, Object> languageMap = new LinkedHashMap<>();
-				if (language != null && !language.trim().isEmpty()) {
-					if (language.length() == 2) {
-						Locale loc = Locale.forLanguageTag(language);
-						languageMap.put("@id", "http://id.loc.gov/vocabulary/iso639-2/"
-								+ loc.getISO3Language());
-					} else if (language.length() == 3) {
-						// vermutlich ISO639-2
-						languageMap.put("@id",
-								"http://id.loc.gov/vocabulary/iso639-2/" + language);
-					} else {
-						play.Logger.warn(
-								"Unbekanntes Vokabluar für Sprachencode! Code=" + language);
-					}
-				}
-				List<Map<String, Object>> languages = new ArrayList<>();
-				languages.add(languageMap);
-				rdf.put("language", languages);
+			if (toscienceJsonObject.has("title")) {
+				jObj = toscienceJsonObject.getJSONObject("title");
+				play.Logger.debug("Found title: " + jObj.toString());
+				rdf.put("title", jObj.toString());
+			}
+
+			if (toscienceJsonObject.has("language")) {
+				jArr = toscienceJsonObject.getJSONArray("language");
+				play.Logger.debug("Found language: " + jArr.toString());
+				rdf.put("language", jArr.toString());
 			}
 
 			rdf.put("accessScheme", "public");
 			rdf.put("publishScheme", "public");
 
-			jArr = toscienceJsonObject.getJSONArray("type");
-			rdf.put("contentType", jArr.getString(0));
+			if (toscienceJsonObject.has("funder")) {
+				jObj = toscienceJsonObject.getJSONObject("funder");
+				play.Logger.debug("Found funder : " + jObj.toString());
+				rdf.put("funder", jObj.toString());
+			}
 
-			List<String> names = new ArrayList<>();
-			names.add(toscienceJsonObject.getString("name"));
-			rdf.put("title", names);
+			// There is always a ContentType
+			jObj = toscienceJsonObject.getJSONArray("contentType");
+			rdf.put("contentType", jObj.toString());
 
-			if (toscienceJsonObject.has("inLanguage")) {
-				List<Map<String, Object>> inLangList = new ArrayList<>();
-				String inLang = null;
-				jArr = toscienceJsonObject.getJSONArray("inLanguage");
-				for (int i = 0; i < jArr.length(); i++) {
-					Map<String, Object> inLangMap = new LinkedHashMap<>();
-					inLang = jArr.getString(i);
-					Locale loc = Locale.forLanguageTag(inLang);
-					inLangMap.put("@id", "http://id.loc.gov/vocabulary/iso639-2/"
-							+ loc.getISO3Language().replace("deu", "ger"));
-					String langPrefLabel = inLang;
-					if (loc.getDisplayLanguage() != null) {
-						langPrefLabel = loc.getDisplayLanguage();
-					}
-					inLangMap.put("prefLabel", langPrefLabel);
-					inLangList.add(inLangMap);
-				}
-				rdf.put("language", inLangList);
+			if (toscienceJsonObject.has("isPrimaryTopic")) {
+				jObj = toscienceJsonObject.getJSONObject("isPrimaryTopic");
+				play.Logger.debug("Found isPrimaryTopic : " + jObj.toString());
+				rdf.put("isPrimaryTopic", jObj.toString());
 			}
 
 			jsmapper.mapLrmiAgentsToLobid(rdf, toscienceJsonObject, "creator");
@@ -110,13 +97,20 @@ public class Metadata2Helper {
 			jsmapper.mapLrmiObjectToLobid(rdf, toscienceJsonObject, "about",
 					"department");
 
+			// Should be mapped?
+			if (toscienceJsonObject.has("yearOfCopyright")) {
+				jObj = toscienceJsonObject.getJSONObject("yearOfCopyright");
+				play.Logger.debug("Found yearOfCopyright : " + jObj.toString());
+				rdf.put("yearOfCopyright", jObj.toString());
+			}
+
 			// template for Mapping of Array
 			if (toscienceJsonObject.has("description")) {
 				List<String> descriptions = new ArrayList<>();
-				myObj = toscienceJsonObject.get("description");
-				if (myObj instanceof java.lang.String) {
+				jObj = toscienceJsonObject.get("description");
+				if (jObj instanceof java.lang.String) {
 					descriptions.add(toscienceJsonObject.getString("description"));
-				} else if (myObj instanceof org.json.JSONArray) {
+				} else if (jObj instanceof org.json.JSONArray) {
 					jArr = toscienceJsonObject.getJSONArray("description");
 					for (int i = 0; i < jArr.length(); i++) {
 						descriptions.add(jArr.getString(i));
@@ -126,46 +120,10 @@ public class Metadata2Helper {
 			}
 
 			if (toscienceJsonObject.has("license")) {
+				jObj = toscienceJsonObject.getJSONObject("license");
+				play.Logger.debug("Found license : " + jObj.toString());
+				rdf.put("license", jObj.toString());
 
-				List<Map<String, Object>> licenses = new ArrayList<>();
-				myObj = toscienceJsonObject.get("license");
-				Map<String, Object> licenseMap = null;
-				if (myObj instanceof java.lang.String) {
-
-					licenseMap = new LinkedHashMap<>();
-					licenseMap.put("@id", toscienceJsonObject.getString("license"));
-					licenses.add(licenseMap);
-				} else if (myObj instanceof org.json.JSONObject) {
-					jObj = toscienceJsonObject.getJSONObject("license");
-
-					licenseMap = new LinkedHashMap<>();
-					licenseMap.put("@id", jObj.getString("id"));
-					licenses.add(licenseMap);
-				} else if (myObj instanceof org.json.JSONArray) {
-					jArr = toscienceJsonObject.getJSONArray("license");
-					for (int i = 0; i < jArr.length(); i++) {
-						jObj = jArr.getJSONObject(i);
-
-						licenseMap = new LinkedHashMap<>();
-						licenseMap.put("@id", jObj.getString("id"));
-						licenses.add(licenseMap);
-					}
-				}
-				rdf.put("license", licenses);
-
-			}
-
-			if (toscienceJsonObject.has("publisher")) {
-				List<Map<String, Object>> institutions = new ArrayList<>();
-				jArr = toscienceJsonObject.getJSONArray("publisher");
-				for (int i = 0; i < jArr.length(); i++) {
-					jObj = jArr.getJSONObject(i);
-					Map<String, Object> publisherMap = new LinkedHashMap<>();
-					publisherMap.put("prefLabel", jObj.getString("name"));
-					publisherMap.put("@id", jObj.getString("id"));
-					institutions.add(publisherMap);
-				}
-				rdf.put("institution", institutions);
 			}
 
 			// example for usage of AdHocUriProvider
@@ -184,21 +142,6 @@ public class Metadata2Helper {
 				rdf.put("subject", subject);
 			}
 
-			// HINT: potential Passepartout for Mapping default JsonObjects
-			if (toscienceJsonObject.has("funder")) {
-
-				// Provide resolving for prefLabels from @id via GenericPropertiesLoader
-				LinkedHashMap<String, String> genPropMap = new LinkedHashMap<>();
-				GenericPropertiesLoader genProp = new GenericPropertiesLoader();
-				genPropMap.putAll(genProp.loadVocabMap("funder-de.properties"));
-
-				jObj = toscienceJsonObject.getJSONObject("funder");
-				Map<String, Object> funderMap = new LinkedHashMap<>();
-				funderMap.put("type", jObj.getString("type"));
-				funderMap.put("@id", jObj.getString("url"));
-				funderMap.put("prefLabel", genPropMap.get(jObj.getString("url")));
-				rdf.put("funder", funderMap);
-			}
 			metadata2Map.put("metadata2", rdf);
 			play.Logger.debug("metadata2Map = " + metadata2Map.toString());
 			play.Logger.debug("Done mapping of toscienceJson to metadata2.");
