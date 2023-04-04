3b43a3ee14d8212f72e6c6cf5c3c8c6e7954c1ab
Ingolf Kuss <kuss@hbz-nrw.de>
2023-04-04 21:15:52 +0200

    deploy.sh und install.sh: Pfade geändert und in ein install/-Verzeichnis geschoben

---
+++ bin/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if (( $EUID == 0 )); then
+    echo "Don't run as root!"
+    exit
+fi
+
+appDeployDir=$(pwd)
+toscienceDir="/opt/toscience"
+deployDir="/opt/toscience/git"
+targetDir="/opt/toscience/apps"
+linkDir="toscience-api"
+fileName="toscience-api-1.0.0-SNAPSHOT.zip"
+folderName="toscience-api-1.0.0-SNAPSHOT"
+newInstallDir="$linkDir.$(date +'%Y%m%d%H%M%S')"
+confDir="/etc/toscience/api"
+
+cp $appDeployDir/target/universal/$fileName $deployDir
+
+cd $deployDir
+unzip $fileName
+
+mv $deployDir/$folderName $targetDir/$newInstallDir
+mkdir $targetDir/$newInstallDir/logs
+
+if [ -L $toscienceDir/$linkDir ]; then
+       	rm $toscienceDir/$linkDir
+fi
+ln -sf $targetDir/$newInstallDir $toscienceDir/$linkDir
+rm -r  $targetDir/$newInstallDir/conf
+ln -sf $confDir $targetDir/$newInstallDir/conf
+
+echo ""
+echo "Neue Binärversion verfügbar unter $targetDir/$newInstallDir."
+echo "Port ist fest eingestellt auf $PLAYPORT"
+echo "Zum Umschalten auf die neue Version:"
+echo "sudo systemctl stop toscience-api.service"
+echo "sudo systemctl start toscience-api.service"
+echo "./loadCache.sh"
