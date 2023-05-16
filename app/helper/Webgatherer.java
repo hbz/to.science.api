1945e4db56fa6f374068353250ffbd358d8c6bd7
Ingolf Kuss <kuss@hbz-nrw.de>
2023-05-16 12:57:54 +0200

    Use java.util.Calendar

--- app/helper/Webgatherer.java
+++ app/helper/Webgatherer.java
@@ -28,13 +28,13 @@ import java.net.URISyntaxException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.ibm.icu.util.Calendar;
 
 import models.Gatherconf;
 import models.Globals;
