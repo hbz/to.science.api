e26e6eb8e52389d161ef11d33671520d7155c9b0
Ingolf Kuss <kuss@hbz-nrw.de>
2023-08-10 19:18:12 +0200

    Verbessere bin/install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -8,7 +8,8 @@ fi
 actDir=$(pwd)
 deployingApp="regal-api"
 toscienceDir="/opt/regal"
-deployDir="/opt/regal/regal-tmp"
+deployDir="$toscienceDir/$deployingApp"
+tmpDir="/opt/regal/regal-tmp"
 targetDir="/opt/regal"
 linkDir="regal-server"
 fileName="regal-api-0.8.0-SNAPSHOT.zip"
@@ -18,14 +19,16 @@ confDir="/etc/to.science/api.conf"
 resourcesDir="/etc/to.science/resources"
 PLAYPORT=9000
 
-cp $deployDir/$deployingApp/target/universal/$fileName $deployDir
+cp $deployDir/target/universal/$fileName $tmpDir
 
-cd $deployDir
+cd $tmpDir
 unzip $fileName
 
-mv $deployDir/$folderName $targetDir/$newInstallDir
+mv $tmpDir/$folderName $targetDir/$newInstallDir
+rm $tmpDir/$fileName
 mkdir $targetDir/$newInstallDir/logs
 
+OLDDIR=`readlink $toscienceDir/$linkDir`
 if [ -L $toscienceDir/$linkDir ]; then
        	rm $toscienceDir/$linkDir
 fi
@@ -40,5 +43,9 @@ echo "Neue Binärversion verfügbar unter $targetDir/$newInstallDir."
 echo "Port ist fest eingestellet auf: $PLAYPORT"
 echo "Zum Umschalten auf die neue Version:"
 echo "sudo service regal-api stop"
+echo "evtl. den noch laufenden Prozess töten:"
+echo "ps -eaf | grep regal-server | more"
+echo "kill `cat $OLDDIR/RUNNING_PID`"
 echo "sudo service regal-api start"
 echo "Das Log unter $targetDir/$newInstallDir/logs/application.log beobachten"
+echo "Warten auf die Meldung \"Success! Regal-API started!\""
