7589f41c754faad462bf08132a14f65e22cfd886
Ingolf Kuss <kuss@hbz-nrw.de>
2024-01-18 11:52:13 +0100

    Use correct install script for toscience-api-1.0.0

--- bin/install.sh
+++ bin/install.sh
@@ -10,10 +10,10 @@ deployingApp="to.science.api"
 toscienceDir="/opt/toscience"
 deployDir="$toscienceDir/git/$deployingApp"
 tmpDir="/opt/toscience/tmp"
-targetDir="/opt/toscience"
+targetDir="/opt/toscience/apps"
 linkDir="toscience-api"
-fileName="regal-api-0.8.0-SNAPSHOT.zip"
-folderName="regal-api-0.8.0-SNAPSHOT"
+fileName="toscience-api-1.0.0-SNAPSHOT.zip"
+folderName="toscience-api-1.0.0-SNAPSHOT"
 newInstallDir="$linkDir.$(date +'%Y%m%d%H%M%S')"
 confDir="/etc/toscience/api"
 resourcesDir="/etc/toscience/resources"
@@ -30,11 +30,12 @@ mkdir $targetDir/$newInstallDir/logs
 
 OLDDIR=`readlink $toscienceDir/$linkDir`
 if [ -L $toscienceDir/$linkDir ]; then
-       	rm $toscienceDir/$linkDir
+        rm $toscienceDir/$linkDir
 fi
 ln -sf $targetDir/$newInstallDir $toscienceDir/$linkDir
 rm -r  $targetDir/$newInstallDir/conf
 ln -sf $confDir $targetDir/$newInstallDir/conf
+cp $deployDir/conf.tmpl/routes $targetDir/$newInstallDir/conf
 ln -sf $resourcesDir $targetDir/$newInstallDir/resources
 cd $actDir
 
@@ -42,10 +43,10 @@ echo ""
 echo "Neue Binärversion verfügbar unter $targetDir/$newInstallDir."
 echo "Port ist fest eingestellet auf: $PLAYPORT"
 echo "Zum Umschalten auf die neue Version:"
-echo "systemctl stop toscience-api.service"
+echo "systemctl stop $linkDir.service"
 echo "evtl. den noch laufenden Prozess töten:"
-echo "ps -eaf | grep toscience-api | more"
+echo "ps -eaf | grep $linkDir | more"
 echo "kill `cat $OLDDIR/RUNNING_PID`"
-echo "systemctl start toscience-api.service"
+echo "systemctl start $linkDir.service"
 echo "Das Log unter $targetDir/$newInstallDir/logs/application.log beobachten"
 echo "Warten auf die Meldung \"Success! Regal-API started!\""
