3bbf3ae81eaedc262f357a7161506e480e533167
Andres Quast <quast@hbz-nrw.de>
2024-09-24 14:18:30 +0200

    add new methods to provide creators with roles in view

--- app/views/Helper.java
+++ app/views/Helper.java
@@ -746,6 +746,7 @@ public class Helper {
 			List<JsonNode> cardNode = jNode.findParents("prefLabel");
 			for (int i = 0; i < cardNode.size(); i++) {
 				String card = cardNode.get(i).findValues("prefLabel").toString() + "; "
+						+ cardNode.get(i).findValues("@id").toString() + "; "
 						+ cardNode.get(i).findValues("role").toString();
 				othersList.add(card.replace("[", "").replace("]", "").replace("\"", "")
 						.replace("_", " "));
