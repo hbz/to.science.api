147bc4c28cdd354b2de904e2713b5545e57a8f9d
Andres Quast <quast@hbz-nrw.de>
2024-09-30 13:12:09 +0200

    generate ktbl resource view as seperate view based on json

--- app/views/Helper.java
+++ app/views/Helper.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -31,6 +32,7 @@ import models.Globals;
 import helper.JsonMdLoader;
 import models.Link;
 import models.Node;
+import play.Logger;
 
 public class Helper {
 
@@ -412,6 +414,14 @@ public class Helper {
 		return result;
 	}
 
+	/**
+	 * This method is buggy and incomplete, please do not use anymore!
+	 * 
+	 * @deprecated
+	 * @param h
+	 * @return
+	 */
+	// TODO: remove method
 	public static List<Map<String, Object>> listContributors(
 			Map<String, Object> h) {
 		List<Map<String, Object>> result = new ArrayList<>();
@@ -640,6 +650,32 @@ public class Helper {
 	}
 
 	/**
+	 * @param node
+	 * @param key
+	 * @return
+	 */
+	public static List<String> getAssociatedDataset(Node node, String key) {
+		String mdStream = getTosJson(node);
+		List<String> valueList = new ArrayList<>();
+		if (mdStream != null) {
+			try {
+				JsonNode jnAll = new ObjectMapper().readTree(mdStream);
+				JsonNode jn = jnAll.findValue(key);
+				Iterator<JsonNode> jIt = jn.elements();
+				while (jIt.hasNext()) {
+					JsonNode nextNode = jIt.next();
+					valueList.add(nextNode.asText().replace("_", " ").replace("\"", ""));
+
+				}
+				return valueList;
+			} catch (IOException e) {
+				play.Logger.warn(e.getMessage());
+			}
+		}
+		return null;
+	}
+
+	/**
 	 * Get complete toscience.json as String
 	 * 
 	 * @param node
@@ -662,7 +698,75 @@ public class Helper {
 	}
 
 	/**
-	 * Get content of Json other-Array from toscience.json
+	 * Get content of Json creator-Array from toscience.json Method to provide
+	 * field "creator" (persons) in correct sequence and with roles from json
+	 * instead of N-triples
+	 * 
+	 * @param node
+	 * @return List
+	 */
+	public static List<String> getCreators(Node node) {
+		String mdStream = getTosJson(node);
+		List<String> creatorsList = new ArrayList<>();
+		JsonNode jNode = null;
+		try {
+			JsonNode jn = new ObjectMapper().readTree(mdStream);
+			jNode = jn.findValue("creator");
+
+			List<JsonNode> cardNode = jNode.findParents("prefLabel");
+			for (int i = 0; i < cardNode.size(); i++) {
+				String card = cardNode.get(i).findValues("prefLabel").toString() + "; "
+				// + cardNode.get(i).findValues("@id").toString() + "; " +
+						+ cardNode.get(i).findValues("role").toString();
+				card = card.replace("[", "").replace("]", "").replace("\"", "")
+						.replace("_", " ").replace(",", ", ");
+				creatorsList.add(card);
+			}
+			return creatorsList;
+		} catch (Exception e) {
+			play.Logger.warn(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Get content of Json contributor-Array from toscience.json Method to provide
+	 * field "contributor" (persons) in correct sequence and with roles from json
+	 * instead of N-triples
+	 * 
+	 * @param node
+	 * @return List
+	 */
+	public static List<String> getContributors(Node node) {
+		String mdStream = getTosJson(node);
+		List<String> contributorsList = null;
+		JsonNode jNode = null;
+		JsonNode jn = null;
+		try {
+			jn = new ObjectMapper().readTree(mdStream);
+			jNode = jn.findValue("contributor");
+		} catch (IOException e) {
+			play.Logger.warn(e.getMessage());
+		}
+
+		if (jNode != null && !jNode.isNull()) {
+			contributorsList = new ArrayList<>();
+			List<JsonNode> cardNode = jNode.findParents("prefLabel");
+			for (int i = 0; i < cardNode.size(); i++) {
+				String card = cardNode.get(i).findValues("prefLabel").toString() + "; "
+				// + cardNode.get(i).findValues("@id").toString() + "; "
+						+ cardNode.get(i).findValues("role").toString();
+				contributorsList.add(card.replace("[", "").replace("]", "")
+						.replace("\"", "").replace("_", " ").replace(",", ", "));
+			}
+		}
+		return contributorsList;
+	}
+
+	/**
+	 * Get content of Json other-Array from toscience.json Method to provide field
+	 * "others" (persons) in correct sequence and with roles from json instead of
+	 * N-triples
 	 * 
 	 * @param node
 	 * @return List
@@ -678,14 +782,38 @@ public class Helper {
 			List<JsonNode> cardNode = jNode.findParents("prefLabel");
 			for (int i = 0; i < cardNode.size(); i++) {
 				String card = cardNode.get(i).findValues("prefLabel").toString() + "; "
+				// + cardNode.get(i).findValues("@id").toString() + "; "
 						+ cardNode.get(i).findValues("role").toString();
 				othersList.add(card.replace("[", "").replace("]", "").replace("\"", "")
-						.replace("_", " "));
+						.replace("_", " ").replace(",", ", "));
 			}
-		} catch (IOException e) {
+			return othersList;
+		} catch (Exception e) {
 			play.Logger.warn(e.getMessage());
 		}
-		return othersList;
+		return null;
+	}
+
+	/**
+	 * Get content of Json other-Array from toscience.json
+	 * 
+	 * @param node
+	 * @param key
+	 * @return List
+	 */
+	public static String getToscienceLiteralValue(Node node, String key) {
+		String literalValue = null;
+		String mdStream = getTosJson(node);
+		if (mdStream != null) {
+			try {
+				JsonNode jn = new ObjectMapper().readTree(mdStream);
+				literalValue = jn.findValue(key).toString().replace("_", " ")
+						.replace("[", "").replace("]", "").replace("\"", "");
+			} catch (IOException e) {
+				play.Logger.warn(e.getMessage());
+			}
+		}
+		return literalValue;
 	}
 
 	/**
@@ -695,22 +823,58 @@ public class Helper {
 	 */
 	public static List<String> getToscienceArrayValues(Node node, String key) {
 		String mdStream = getTosJson(node);
-		List<String> valueList = new ArrayList<>();
+		List<String> valueList = null;
 		if (mdStream != null) {
 			try {
 				JsonNode jnAll = new ObjectMapper().readTree(mdStream);
 				JsonNode jn = jnAll.findValue(key);
-				Iterator<JsonNode> jIt = jn.elements();
-				while (jIt.hasNext()) {
-					JsonNode nextNode = jIt.next();
-					valueList.add(nextNode.asText().replace("\\u2019", "'"));
+				if (jn != null && !jn.isNull()) {
+					valueList = new ArrayList<>();
 
+					Iterator<JsonNode> jIt = jn.elements();
+					while (jIt.hasNext()) {
+						JsonNode nextNode = jIt.next();
+						valueList.add(nextNode.asText().replace("\u2019", "'"));
+					}
 				}
+
 			} catch (IOException e) {
 				play.Logger.warn(e.getMessage());
 			}
 		}
 		return valueList;
+	}
+
+	/**
+	 * Get content of an Array of objects with prefLabel from toscience.json
+	 * 
+	 * @param node
+	 * @param key
+	 * @return List
+	 */
+	public static List<Hashtable<String, String>> getToscienceObjectArray(
+			Node node, String key) {
+		List<Hashtable<String, String>> valueList = new ArrayList<>();
+		String mdStream = getTosJson(node);
+		JsonNode jNode = null;
+		try {
+			JsonNode jn = new ObjectMapper().readTree(mdStream);
+			jNode = jn.findValue(key);
+
+			List<JsonNode> cardNode = jNode.findParents("prefLabel");
+			for (int i = 0; i < cardNode.size(); i++) {
+				Hashtable<String, String> valueHash = new Hashtable<>();
+				valueHash.put("prefLabel", cardNode.get(i).findValues("prefLabel")
+						.toString().replace("[", "").replace("]", "").replace("\"", ""));
+				valueHash.put("id", cardNode.get(i).findValues("@id").toString()
+						.replace("[", "").replace("]", "").replace("\"", ""));
+				valueList.add(valueHash);
+			}
+			return valueList;
+		} catch (Exception e) {
+			play.Logger.warn(e.getMessage());
+		}
+		return null;
 	}
 
 	/**
@@ -823,7 +987,7 @@ public class Helper {
 				JsonNode jn = new ObjectMapper().readTree(mdStream);
 				value = jn.findValue("ventilation_system").toString().replace("_", " ")
 						.replace("\"", "");
-			} catch (IOException e) {
+			} catch (Exception e) {
 				play.Logger.warn(e.getMessage());
 			}
 		}
@@ -843,7 +1007,8 @@ public class Helper {
 			try {
 				JsonNode jn = new ObjectMapper().readTree(mdStream);
 				value = jn.findValue("project_title").toString().replace("_", " ")
-						.replace("\"", "");
+						.replace("\"", "").replace("emim", "EmiM")
+						.replace("emidat", "EmiDaT");
 			} catch (IOException e) {
 				play.Logger.warn(e.getMessage());
 			}
@@ -869,11 +1034,12 @@ public class Helper {
 					valueList.add(nextNode.asText().replace("_", " ").replace("\"", ""));
 
 				}
-			} catch (IOException e) {
+				return valueList;
+			} catch (Exception e) {
 				play.Logger.warn(e.getMessage());
 			}
 		}
-		return valueList;
+		return null;
 	}
 
 	/**
@@ -896,11 +1062,12 @@ public class Helper {
 							.replace("4", "₄").replace("DOUR", "dour"));
 
 				}
+				return valueList;
 			} catch (IOException e) {
 				play.Logger.warn(e.getMessage());
 			}
 		}
-		return valueList;
+		return null;
 	}
 
 	/**
@@ -912,4 +1079,12 @@ public class Helper {
 		return mdStream.toString().trim().replace("&quot;", "\"");
 	}
 
+	/**
+	 * @param mdStream
+	 * @return
+	 */
+	public static String capitalizeFirst(String value) {
+
+		return value.substring(0, 0).toUpperCase() + value.substring(1);
+	}
 }
