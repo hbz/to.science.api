7086fc29d3e6b9c0c78f5ae8d75c4772851b2f53
Andres Quast <quast@hbz-nrw.de>
2024-09-30 18:28:31 +0200

    fix roles list

--- app/views/KtblHelper.java
+++ app/views/KtblHelper.java
@@ -235,10 +235,12 @@ public class KtblHelper {
 				valueHash.put("id", cardNode.get(i).findValues("@id").toString()
 						.replace("[", "").replace("]", "").replace("\"", ""));
 				if (cardNode.get(i).has("role")) {
-					valueHash.put("roles",
-							cardNode.get(i).findValues("role").toString().replace("[", "")
-									.replace(",", ", ").replace("]", "").replace("\"", "")
-									.replace("_", " "));
+					String roles = "";
+					List<String> roleList = cardNode.get(i).findValuesAsText("role");
+					for (int k = 0; i < roleList.size(); i++) {
+						roles = roles + ", " + roleList.get(k).replace("-", " ");
+					}
+					valueHash.put("roles", roles);
 				}
 				valueList.add(valueHash);
 			}
