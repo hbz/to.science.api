815b1d5486430b1c742b96777ac86a7fb4c8d9ec
Ingolf Kuss <kuss@hbz-nrw.de>
2026-06-08 17:37:15 +0200

    Debug regExp search for archive name

--- app/helper/BtrixCrawlIngestArchive.java
+++ app/helper/BtrixCrawlIngestArchive.java
@@ -122,9 +122,9 @@ public class BtrixCrawlIngestArchive extends CrawlerModelIngestArchive {
 				String archiveFiles[] = archiveDir.list(new FilenameFilter() {
 					@Override
 					public boolean accept(File d, String name) {
-						WebgatherLogger.debug("Found archive file or dir" + d.getName());
+						WebgatherLogger.debug("Found archive file or dir " + d.getName());
 						if (!d.isFile()) {
-							return false;
+							// return false;
 						}
 						WebgatherLogger.debug("Found archive file " + name);
 						Matcher matcher = pattern.matcher(name);
