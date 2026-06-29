ea06e2e64e812fb67495d8e9ae3203ec6ee16764
Ingolf Kuss <kuss@hbz-nrw.de>
2026-06-29 14:11:37 +0200

    octicon elems remove

--- app/helper/TosHelper.java
+++ app/helper/TosHelper.java
@@ -15,7 +15,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Elements;
+import org.jsoup.select.Elements;
 import org.jsoup.Jsoup;
 
 import models.Globals;
@@ -239,9 +239,10 @@ public class TosHelper {
 		try {
 			org.jsoup.nodes.Document doc = org.jsoup.Jsoup.parse(content);
 			Elements octiconElems = doc.getElementsByClass("octicon");
-			for (Element element : doc.select(octiconElems)) {
-				element.remove();
-			}
+			octiconElems.remove();
+			// for (Element element : doc.select(octiconElems)) {
+			// element.remove();
+			// }
 			play.Logger.debug("Bereinigtes treeHtml: " + doc.html());
 			return doc.html();
 		} catch (Exception e) {
