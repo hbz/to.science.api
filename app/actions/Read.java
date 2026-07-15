11d5a39c1786eca5763040b79e9abfd6ef7fa49f
Ingolf Kuss <kuss@hbz-nrw.de>
2026-07-16 00:32:17 +0200

    Leeren openWaybackLink in getConf nicht erzeugen; Webpages haben keinen openWaybackLink!

--- app/actions/Read.java
+++ app/actions/Read.java
@@ -629,18 +629,6 @@ public class Read extends RegalAction {
 				return "";
 			ObjectMapper mapper = JsonUtil.mapper();
 			Gatherconf conf = mapper.readValue(confstring, Gatherconf.class);
-			if (conf.getOpenWaybackLink() == null
-					|| conf.getOpenWaybackLink().isEmpty()) {
-				play.Logger.debug("conf.openWaybackLink is empty");
-				String owDatestamp =
-						new SimpleDateFormat("yyyyMMdd").format(conf.getStartDate());
-				play.Logger.debug("owDatestamp: " + owDatestamp);
-				String collection = conf.fetchCollection();
-				play.Logger.debug("collection: " + collection);
-				conf.setOpenWaybackLink(Play.application().configuration()
-						.getString("regal-api.wayback.collection." + collection)
-						+ owDatestamp + "/" + conf.getUrl());
-			}
 			return conf.toString();
 		} catch (UrlConnectionException e) {
 			play.Logger.error("Url Connection Exception", e);
