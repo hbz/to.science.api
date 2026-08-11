c904bfb1885a29dcd470c6d68c49a7f2ae3c8adc
hadoud <hasan.adoud@hbz-nrw.de>
2026-08-11 14:14:05 +0200

    Correcting the Display of Roles

--- app/views/Helper.java
+++ app/views/Helper.java
@@ -866,8 +866,10 @@ public class Helper {
 				valueHash.put("id", cardNode.get(i).findValues("@id").toString()
 						.replace("[", "").replace("]", "").replace("\"", ""));
 				if (cardNode.get(i).has("role")) {
-					valueHash.put("roles", cardNode.get(i).findValues("role").toString()
-							.replace("[", "").replace("]", "").replace("\"", ""));
+					valueHash.put("roles",
+							cardNode.get(i).findValues("role").toString().replace("[", "")
+									.replace("]", "").replace("\"", "").replace(",", ", ")
+									.replace("_", " "));
 				}
 				valueList.add(valueHash);
 			}
